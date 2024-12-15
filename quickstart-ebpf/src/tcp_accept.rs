8290ea7614bba9b09d9bf41ed9b9b866f673b9ea
wangrushen <wrs369@88.com>
2024-12-15 23:53:03 +0800

    print comm and addr in tcpaccpt

--- quickstart-ebpf/src/tcp_accept.rs
+++ quickstart-ebpf/src/tcp_accept.rs
@@ -2,10 +2,10 @@
 #![no_main]
 mod vmlinux;
 
-use core::mem::offset_of;
+use core::{mem::offset_of, str::{from_utf8, from_utf8_unchecked}};
 
 use aya_ebpf::{
-    helpers::{bpf_get_current_pid_tgid, bpf_probe_read_kernel},
+    helpers::{bpf_get_current_comm, bpf_get_current_pid_tgid, bpf_probe_read_kernel},
     macros::kretprobe,
     programs::ProbeContext,
 };
@@ -21,14 +21,19 @@ pub fn kretprobe_inet_csk_accept(ctx: ProbeContext) -> u32 {
     }
 }
 
+macro_rules! filed_of {
+    ($ptr:ident ,$ty:ty, $field:ident) => {{
+        let filed_offset = offset_of!($ty, $field);
+        unsafe { bpf_probe_read_kernel(($ptr as usize + filed_offset) as *const _) }?
+    }};
+}
+
 fn try_inet_csk_accept(ctx: ProbeContext) -> Result<u32, i64> {
-    info!(&ctx, "kretprobe_inet_csk_accept");
     let sk_ptr: *const sock = ctx.ret().ok_or(0)?;
+
     let pid = (bpf_get_current_pid_tgid() >> 32) as u32;
-    let skc = unsafe { bpf_probe_read_kernel(sk_ptr as *const sock_common) }?;
-    
-    let field_offset = offset_of!(sock, sk_protocol);
-    let protocol = unsafe { bpf_probe_read_kernel((sk_ptr as usize + field_offset) as *const u16) }?;
+    let skc: sock_common = filed_of!(sk_ptr, sock, __sk_common);
+    let protocol: u16 = filed_of!(sk_ptr, sock, sk_protocol);
 
     if protocol != IpProto::Tcp as u16 {
         return Ok(0);
@@ -38,11 +43,23 @@ fn try_inet_csk_accept(ctx: ProbeContext) -> Result<u32, i64> {
     let lport = unsafe { skc.__bindgen_anon_3.__bindgen_anon_1.skc_num };
     let dport = unsafe { skc.__bindgen_anon_3.__bindgen_anon_1.skc_dport };
 
+    let saddr = unsafe { skc.__bindgen_anon_1.__bindgen_anon_1.skc_rcv_saddr };
+    let daddr = unsafe { skc.__bindgen_anon_1.__bindgen_anon_1.skc_daddr };
+
+    let comm = bpf_get_current_comm()?;
+    
+    let comm_str = unsafe { from_utf8_unchecked(&comm) };
     info!(
         &ctx,
-        "accepted: pid: {}, family: {}, lport: {}, dport: {}", pid, family, lport, dport
+        "accepted: pid: {}, family: {}, lport: {}, dport: {}, saddr: {}, daddr: {}, comm: {}",
+        pid,
+        family,
+        lport,
+        dport,
+        saddr,
+        daddr,
+        comm_str,
     );
-
     Ok(0)
 }
 
